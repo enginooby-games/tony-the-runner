de8d838b256b9169db2f2aaed8c8518b2f5e19de
Hieu Ngo <justacpulover@gmail.com>
2021-01-09 23:56:09 +0100

    Implement infinite moving background

---
+++ assets/scripts/Background.ts
@@ -0,0 +1,40 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+    @property
+    speed: number = 100
+
+    start() {
+
+    }
+
+    move(node, offset) {
+        // find the X coord of the right edge of the current sprite
+        const spriteRightX = node.x + node.width / 2;
+        // find the X coord of the left edge of the screen
+        const screenLeftX = -cc.winSize.width / 2;
+
+        // if the right X of the sprite is less than left X of the screen
+        if (spriteRightX <= screenLeftX) {
+            // swap the images
+            node.x += node.width * 2 - offset;
+        } else {
+            // else shift current node with the specified offset
+            node.x -= offset;
+        }
+    }
+
+    update(dt) {
+        this.node.children.forEach(node => {
+            this.move(node, dt * this.speed);
+        });
+    }
+}
